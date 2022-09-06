4071aca146b354dcfecfb399e6e95241cd9744b6
Kathrin Ayer <kathrin.ayer@gmail.com>
2022-09-06 09:26:38 -0400

    add 404 handler
    Added quick 404 handler for no route found.

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,9 +7,9 @@ import deutschRouter from './routes/deutsch';
 dotenv.config();
 
 const app = express();
-const port = 3000;
 
 app.use(express.json()); // to support JSON-encoded bodies
+app.use(cors());
 
 app.use(
   express.urlencoded({
@@ -17,7 +17,6 @@ app.use(
     extended: true,
   }),
 );
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Welcome to your server');
@@ -25,12 +24,18 @@ app.get('/', (req, res) => {
 
 app.use('/de', deutschRouter);
 
+app.use((_, res) => {
+  res.status(404);
+  res.send('404: File Not Found');
+});
+
 export default app;
 
 /*
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://verbAPI_dbUser:<password>@cluster0.28qnidw.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import mongoose from 'mongoose';
 
@@ -5,13 +6,14 @@ import app from './app';
 
 dotenv.config();
 
-let server: any;
+let server: unknown;
 const port = 3000;
 
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
     console.log('Successfully connected to MongoDB Atlas!');
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     server = app.listen(port, () => {
       console.log('app is listening');
     });
