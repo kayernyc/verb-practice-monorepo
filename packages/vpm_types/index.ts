d1d0c0fa7ffdde6d8d3be774fbb526bb44370303
Kathrin <gypsy_kat@yahoo.com>
2023-04-21 11:31:27 +0200

    reorganize files
    Changed "seed" to "db_management" and updated tsconfig to consume base configuration.

---
+++ packages/vpm_types/index.ts
@@ -0,0 +1,9 @@
+import * as JsonTypes from './jsonTypes';
+import * as EnglishConstants from './englishConstants';
+import * as EnglishTypes from './englishTypes';
+
+export const VPM_Types = {
+  JsonTypes,
+  EnglishConstants,
+  EnglishTypes
+}
