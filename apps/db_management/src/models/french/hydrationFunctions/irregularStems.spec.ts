ba3aed30c7eec9cacf4dd8543ae7c0cd8be30cfc
Kathrin Ayer <kathrin.ayer@gmail.com>
2023-07-22 13:28:59 -0400

    fix: delete suite with no test

--- apps/db_management/src/models/french/hydrationFunctions/irregularStems.spec.ts
+++
@@ -1,7 +0,0 @@
-import { createStandardConjugation } from "@models/french/createStandardConjugation";
-import { irregularStems } from "@models/french/hydrationFunctions/irregularStems";
-import { IrregularStems } from "@models/french/types/hydrationTypes";
-
-describe.skip('irregularStems', () => {
-
-});
