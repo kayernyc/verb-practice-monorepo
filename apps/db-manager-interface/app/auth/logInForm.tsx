1b238ac9245adc0c1f2ba1923e10af11a7be06b7
Kathrin <kathrin.ayer@gmail.com>
2023-08-21 17:31:59 -0400

    feat: save the change
    Saved the change.

--- apps/db-manager-interface/app/auth/logInForm.tsx
+++ apps/db-manager-interface/app/auth/logInForm.tsx
@@ -23,10 +23,7 @@ export const LogInForm = () => {
       const result = await (
         await fetch('http://localhost:3030/auth/signin', {
           method: 'post',
-<<<<<<< HEAD
           credentials: 'include',
-=======
->>>>>>> main
           headers: {
             'content-type': 'application/json',
           },
